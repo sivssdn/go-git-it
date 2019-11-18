2bf512d98b37b1f4f26c8b081d9ddd8dad7ff657
Paras <sivssdn@gmail.com>
2019-11-18 15:45:26 +0530

    autocomplete '*' identifier bug resolved

--- src/branch.go
+++ src/branch.go
@@ -12,16 +12,14 @@ import (
 func AutoCompleteBranchName(name string) string {
 	command := exec.Command("git", "branch", "-a")
 	commandOutput, err := command.Output()
-
 	if err != nil {
 		panic(err)
 	}
-	branchesArray := strings.Split(string(commandOutput), " ")
-	for i := 1; i < len(branchesArray); i += 2 {
+	branchesArray := strings.Split(strings.Replace(string(commandOutput), "\n", " ", -1), " ")
+	for i := 1; i < len(branchesArray); i++ {
 		if strings.Contains(branchesArray[i], name) {
 			return branchesArray[i]
 		}
 	}
-
 	return "Branch Not Found"
 }
